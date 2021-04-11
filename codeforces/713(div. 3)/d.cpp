7df5dc9254e9fb154fdf055e3672c2f63bd07a8e
Joowon Lim <m4rinesnow34@gmail.com>
2021-04-11 11:57:49 +0000

    codeforce 713(div.3) / (after virtual) d

--- codeforces/713(div. 3)/d.cpp
+++ codeforces/713(div. 3)/d.cpp
@@ -16,12 +16,34 @@ int main(){
     }
     sort(map.begin(),map.end());
 
-    int sum1=0,sum2=0;
+    long long sum1=0,sum2=0;
     for(int i=0;i<n;i++){
       sum1+= map[i];
     }
-    sum2 += map[n] - map[0];
-    printf("%d %d\n", sum1,sum2);
+    if(sum1 == map[n] || sum1 == map[n+1]){
+      for(int i =0; i<n;i++){
+        printf("%d ", map[i]);
+      }
+      printf("\n");
+    }
+    else{
+      int jud=0;
+      for(int i = 0;i<n;i++){
+        sum2 = sum1 - map[i] + map[n];
+        if(sum2 == map[n+1]){
+          for(int j =0; j<n+1;j++){
+            if(i==j) continue;
+            printf("%d ", map[j]);
+          }
+          printf("\n");
+          jud = 1;
+          break;
+        }
+      }
+      if(jud == 0){
+        printf("-1\n");
+      }
+    }
   }
   return 0;
 }
